f05e266b964a226533267032f7f574c2e94a68b6
OmBhut44 <obhut0004@gmail.com>
2026-01-20 12:07:10 +0530

    Query parameter -> used when you want to do add  searching, filtering and pagination in your existing end point

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 # FastAPI -> to create the API
 # Path -> to validate path parameters
+# Query -> to validate query parameters
+# HTTPException -> to handle exceptions
 
 import json
 # to handle json data
@@ -37,3 +39,20 @@ def view_patient(patient_id: str = Path(..., description="The ID of the patient 
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404, detail="Patient not found")
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description="sort on the basic of height, weight or BMI"), order : str = Query("asc", description="sort in ascending or descending order")):
+
+
+    valid_fields = ["height", "weight", "bmi"]
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f"Invalid field. Must be one of {valid_fields}")
+    
+    if order not in ["asc", "desc"]:
+        raise HTTPException(status_code=400, detail="Invalid order. Must be 'asc' or 'desc'")
+    
+    data = load_data()
+    sort_order = True if order == "desc" else False
+    sorted_data = sorted(data.values(), key=lambda x: x[sort_by], reverse=sort_order)
+
+    return {"sorted_patients": sorted_data}
